ba97cf9fed9e4c765483e98d0c9878bde69513ec
Dromach <90900502+Dromach@users.noreply.github.com>
2021-10-27 19:25:00 +0200

    Create Contact.vue

---
+++ components/Contact.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="shadow-md bg-white rounded-md p-3 text-center flex flex-shrink flex-grow md:flex-grow-0">
+        <form class="">
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-nom">
+                        Votre Nom
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Phillipe Chateaux">
+                </div>
+                <div class="w-full md:w-1/2 px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-mail">
+                        Votre mail
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="xyz@mail.com">
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+                <div class="w-full px-3">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-tel">
+                        Téléphone
+                    </label>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="telephone" placeholder="06 75 26 84 20">
+                </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-2">
+                <div class="w-full px-3 mb-6 md:mb-0">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-msg">
+                        Votre message
+                    </label>
+                    <textarea name="your-message" cols="100" rows="10" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" aria-invalid="false" spellcheck="false"></textarea>
+                </div>
+            </div>
+            <div>
+                <input type="submit" value="Envoyer" class="bg-red-500 rounded-md w-6/12">
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['contact'],
+}
+</script>
