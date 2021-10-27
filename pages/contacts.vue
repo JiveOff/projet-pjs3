8df8521256effb662d2ea189faed8a7ce9ec9ce1
Dromach <90900502+Dromach@users.noreply.github.com>
2021-10-27 19:24:26 +0200

    Create contacts.vue

---
+++ pages/contacts.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div
+			class="bg-white rounded-md rounded-tr-none rounded-tl-none text-center shadow-md mb-5 pb-4"
+		>
+			<div class="text-3xl p-2 pt-6 font-semibold tracking-widest uppercase">
+				Nous contacter
+			</div>
+		</div>
+        <div
+            class="flex justify-center flex-wrap gap-5 p-5 rounded-md bg-fixed"
+            :style="{
+                'background-image': `url(${require('@/assets/images/brehat.png')})`,
+                'background-size': 'cover',
+                'background-repeat': 'no-repeat',
+                'background-position': '50% -116px',
+            }"
+        >
+            <Contact />
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'contacts',
+}
+</script>
